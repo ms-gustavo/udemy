594157976efc78fa953ccee928047d06e61f131a
Gustavo Maia <109868838+gugarioms@users.noreply.github.com>
2022-12-20 19:39:02 -0300

    miniblog udemy pt2

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,112 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const navigate = useNavigate();
+  const user = useAuthValue();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    //validar URL image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // criar o array de Tags
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError(`Por favor, preencha todos os campos.`);
+    }
+    if (formError) return;
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to homepage
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Create Post</h1>
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Titulo:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <input
+            type="textarea"
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Pense em uma imagem que represente o seu post..."
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as Tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+
+        {!response.loading && <button className="btn">Criar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   );
 };
